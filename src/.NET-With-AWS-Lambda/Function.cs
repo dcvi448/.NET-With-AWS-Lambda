31f3f6db100fe6e9e9bf8857221c291ef25ca3cd
ngtduc693 <ngtduc693@gmail.com>
2023-04-15 10:43:48 +0700

    init

--- src/.NET-With-AWS-Lambda/Function.cs
+++ src/.NET-With-AWS-Lambda/Function.cs
@@ -1,21 +1,50 @@
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
 using Amazon.Lambda.Core;
+using HtmlAgilityPack;
+using Newtonsoft.Json;
 
-// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
 namespace _NET_With_AWS_Lambda;
 
+
 public class Function
 {
-    
-    /// <summary>
-    /// A simple function that takes a string and does a ToUpper
-    /// </summary>
-    /// <param name="input"></param>
-    /// <param name="context"></param>
-    /// <returns></returns>
-    public string FunctionHandler(string input, ILambdaContext context)
+
+    private static readonly HttpClient _httpClient = new HttpClient();
+    private const string _url = "";
+    public async Task<string> FunctionHandler(ILambdaContext context)
     {
-        return input.ToUpper();
+        var html = await _httpClient.GetStringAsync(_url);
+
+        var doc = new HtmlDocument();
+        doc.LoadHtml(html);
+
+        var tables = doc.DocumentNode.SelectNodes("//table//tbody");
+
+        var result = new List<Dictionary<string, string>>();
+
+        foreach (var table in tables)
+        {
+            var rows = table.SelectNodes("tr");
+
+            foreach (var row in rows)
+            {
+                var cells = row.SelectNodes("td");
+
+                if (cells != null && cells.Count > 1)
+                {
+                    var rowDict = new Dictionary<string, string>();
+                    rowDict.Add(cells[0].InnerText.Trim(), cells[1].InnerText.Trim());
+                    result.Add(rowDict);
+                }
+            }
+        }
+
+        return JsonConvert.SerializeObject(result);
     }
+
 }
